a8bf20001d0a4a2467a6c6f302fbf50dfbe88e17
PETER <peter@MacBook-Pro.local>
2026-02-26 14:30:33 +0100

    Update port and Add debouce delay to dropdown suggestions

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ import rateLimit from "express-rate-limit"
 const app = express();
 app.set("trust proxy", 1); //enables express to see the real user IP for ratelimit//
 
-const PORT =  3000;
+const PORT = process.env.PORT || 3000;
 
 
 const limiter = rateLimit({
